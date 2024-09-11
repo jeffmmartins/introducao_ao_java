fe3692e5f0075cda72877dc3e6036d8c0bb1cba5
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-11 18:57:59 -0300

    Atribuindo uma Variavel

--- src/Main.java
+++ src/Main.java
@@ -2,10 +2,9 @@
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-        //TIP Press <shortcut actionId="ShowIntentionActions"/> with your caret at the highlighted text
-        // to see how IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello world");
+        String nome = "Jefferson";
 
+        System.out.println("Olá" + nome);
 
     }
 }

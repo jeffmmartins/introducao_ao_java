1a45a25c0d43e943410afdacc18a805e97c0996e
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-14 18:40:55 -0300

    estruturas condicionas

---
+++ src/estruturas_condicionais.java
@@ -0,0 +1,10 @@
+public class estruturas_condicionais {
+    public static void main(String[] args) {
+        int nota = 50;
+        if (nota >= 70) {
+            System.out.println("Aluno aprovado");
+        } else {
+            System.out.println("aluno reprovado");
+        }
+    }
+}

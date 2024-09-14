7729a6e88f20cd778ef22f92008226765058a663
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-14 18:12:31 -0300

    operadores logicos

---
+++ src/operadores_logivo.java
@@ -0,0 +1,9 @@
+public class operadores_logivo {
+    public static void main(String[] args) {
+        boolean fimDeSemana = true;
+        boolean vamosAPraia = true;
+        boolean resultado = fimDeSemana && vamosAPraia;
+
+        System.out.println(resultado);
+    }
+}

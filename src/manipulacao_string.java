dffc7d40945851739e5cb3c4a0e1f8fc266a260d
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-14 20:51:35 -0300

    manipulação de string

---
+++ src/manipulacao_string.java
@@ -0,0 +1,9 @@
+import java.time.LocalDate;
+
+public class manipulacao_string {
+    public static <localDate> void main(String[] args) {
+        // Recuperando a data
+        LocalDate hoje = LocalDate.now();
+        System.out.println(hoje);
+    }
+}

e0cc2ebb9d3c71e44cfd58abf10511a96e5ebe9b
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-15 10:02:41 -0300

    função

---
+++ src/funcao.java
@@ -0,0 +1,9 @@
+public class funcao {
+    public static void main(String[] args) {
+       saudacao();
+    }
+
+    public static void saudacao(){
+        System.out.println("Hello, Jeff");
+    }
+}

9e88f9964efe37b6d428d28cc0d84d7c75b5673a
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-15 09:55:57 -0300

    exemplo de vetores finalizado

--- src/vetores.java
+++ src/vetores.java
@@ -1,3 +1,5 @@
+import java.util.Arrays;
+
 public class vetores {
     public static void main(String[] args) {
         int[] numeros = new int[5];
@@ -13,8 +15,29 @@ public class vetores {
 
         String[] letras = {"a","b", "d", "f"};
 
-        for (int i = 0; i < letras.length; i++){
-            System.out.println(letras[i]);
+        //for (int i = 0; i < letras.length; i++){
+         //   System.out.println(letras[i]);
+        //}
+
+        //outra forma de imprimir o arrays
+        System.out.println(Arrays.toString(letras));
+
+        int[] numero = {1, 5, 54, 63, 2};
+        int maior = numero[0];
+        int menor = numero[0];
+        int media = 0;
+
+        for (int i = 0;i < numero.length; i++){
+            if (numero[i] > maior){
+                maior = numero[i];
+            }
+            if (numero[i] < menor){
+                menor = numero[i];
+            }
+            media += numero[i] / numeros.length;
         }
+        System.out.println("Maior: " + maior);
+        System.out.println("Menor: " + menor);
+        System.out.println("Media: " + media);
     }
 }

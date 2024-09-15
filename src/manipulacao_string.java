4b2aa408f8254a4c122d3dd508db9463166b1a37
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-14 21:19:57 -0300

    exemplo de manipulação e string finalizado

--- src/manipulacao_string.java
+++ src/manipulacao_string.java
@@ -1,9 +1,29 @@
 import java.time.LocalDate;
+import java.time.format.TextStyle;
+import java.util.Locale;
+import java.time.LocalDateTime;
 
 public class manipulacao_string {
     public static <localDate> void main(String[] args) {
         // Recuperando a data
+        // Recuperando a data atual
         LocalDate hoje = LocalDate.now();
         System.out.println(hoje);
+
+        // Definindo o Locale para o Brasil
+        Locale brasil = new Locale("pt", "BR");
+
+        // Exibindo o nome completo do dia da semana em português
+        String diaSemana = hoje.getDayOfWeek().getDisplayName(TextStyle.FULL, brasil);
+        System.out.println(diaSemana);
+
+        LocalDateTime agora = LocalDateTime.now();
+        String saudacao;
+        if (agora.getHour() > 8 && agora.getHour() < 12) {
+            saudacao = "Bom dia";
+        }else {
+            saudacao = "boa noite";
+        }
+        System.out.println(saudacao);
     }
 }

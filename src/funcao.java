0100f48b3cb9d26ba0d51ab3a9d16d097c596ae7
Jefferson Martins Mendes <jefferson_mm_@outlook.com>
2024-09-15 10:25:08 -0300

    metodo com retorno de valor

--- src/funcao.java
+++ src/funcao.java
@@ -2,9 +2,16 @@ public class funcao {
     public static void main(String[] args) {
         String nomeOriginal = "Jeff";
         saudacao(nomeOriginal);
+        int resultado = soma(2,3);
+        System.out.println(resultado);
     }
 
     public static void saudacao(String nome){
         System.out.println("Hello " + nome);
     }
+
+    public static int soma(int a, int b){
+        return a + b;
+    }
+
 }
